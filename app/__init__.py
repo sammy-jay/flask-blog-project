8a321de5c6876f8878f7e4335faed373eb8291de
sammy-jay <sammyjayden27@gmail.com>
2022-08-20 19:57:23 +0100

    auth bp registered, schema initialized

--- app/__init__.py
+++ app/__init__.py
@@ -26,4 +26,9 @@ def create_app(test_config=None):
     def hello():
         return "Hello, world!"
     
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
     return app
