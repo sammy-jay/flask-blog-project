b14514114c81e7921620664e11ee16e9e5d93f33
sammy-jay <sammyjayden27@gmail.com>
2022-08-20 22:38:52 +0100

    added more input validation

--- app/auth.py
+++ app/auth.py
@@ -67,6 +67,7 @@ def login():
 @bp.before_app_request
 def load_logged_in_user():
     user_id = session.get('user_id')
+    
 
     if user_id is None:
         g.user = None
@@ -74,6 +75,7 @@ def load_logged_in_user():
         g.user = get_db().execute(
             'SELECT * FROM user WHERE id = ?', (user_id,)
         ).fetchone()
+        
 
 
 @bp.route("/logout")
